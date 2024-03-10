5ff4d7e4e163e5e9e66409986149cdd8d035b083
Xue-Zheng <Xue.Zheng@lincolnuni.ac.nz>
2024-03-10 21:16:47 +1300

    Fix Database

--- connect.py
+++ connect.py
@@ -1,5 +1,6 @@
 import mysql.connector
 conn = mysql.connector.connect(
+    port='3306',
     host='localhost',
     user='root',
     password='Clara521521',
--- init.sql
+++ init.sql
@@ -1,42 +1,42 @@
-insert into AnimalPest (animal_id, description, size)
-values (
-        'Asian Brown Mussel',
-        'Brown or brown-yellow color, possibly tinged with green around the edges.',
-        'Length of about 90mm, can reach up to 120mm.'
-    ),
-    (
-        'Crab with Mitten Claws',
-        ' Front claws with hairy mitten with white tips, distinctive notches between the eyes, 4 spines on each side of the carapace, light brown to olive green carapace.',
-        'Carapace width generally 5cm to 7cm, up to 10cm.'
-    ),
-    (
-        'South American Fruit Fly',
-        'Distinctive patterned wings with yellow to orange-brown bands, yellow to orange-brown body.',
-        '12mm to 14mm long.'
-    ),
-    (
-        'Aphid',
-        'Pale green, slightly elongated, oval-shaped.',
-        'About 2mm long.'
-    ),
-    (
-        'Asian Ambrosia Beetle',
-        'The adult beetles are small and slender, with a brown-black color, and are hard to spot as they spend most of their lives inside their tunnels.',
-        '2mm to 3mm long.'
-    );
-insert into AnimalPest (animal_id)
-values ('citrus longhorn beetle'),
-    ('Pea Weevil'),
-    ('Mountain pine Beetle'),
-    ('Black Rot'),
-    ('Brown Marmorated Stink Bug'),
-    ('Flies'),
-    ('Pine Shoot Beetle'),
-    ('European Pine Shoot Moth'),
-    ('Red Imported Fire Ant (RIFA)'),
-    ('Painted Apple Moth Caterpillar'),
-    ('Pine Processionary Moth'),
-    ('European Crane Fly'),
-    ('Wallaby'),
-    ('Northern Pacific Seastar'),
-    ('Nun Moth');
+-- insert into AnimalPest (animal_id, description, size)
+-- values (
+--         'Asian Brown Mussel',
+--         'Brown or brown-yellow color, possibly tinged with green around the edges.',
+--         'Length of about 90mm, can reach up to 120mm.'
+--     ),
+--     (
+--         'Crab with Mitten Claws',
+--         ' Front claws with hairy mitten with white tips, distinctive notches between the eyes, 4 spines on each side of the carapace, light brown to olive green carapace.',
+--         'Carapace width generally 5cm to 7cm, up to 10cm.'
+--     ),
+--     (
+--         'South American Fruit Fly',
+--         'Distinctive patterned wings with yellow to orange-brown bands, yellow to orange-brown body.',
+--         '12mm to 14mm long.'
+--     ),
+--     (
+--         'Aphid',
+--         'Pale green, slightly elongated, oval-shaped.',
+--         'About 2mm long.'
+--     ),
+--     (
+--         'Asian Ambrosia Beetle',
+--         'The adult beetles are small and slender, with a brown-black color, and are hard to spot as they spend most of their lives inside their tunnels.',
+--         '2mm to 3mm long.'
+--     );
+-- insert into AnimalPest (animal_id)
+-- values ('citrus longhorn beetle'),
+--     ('Pea Weevil'),
+--     ('Mountain pine Beetle'),
+--     ('Black Rot'),
+--     ('Brown Marmorated Stink Bug'),
+--     ('Flies'),
+--     ('Pine Shoot Beetle'),
+--     ('European Pine Shoot Moth'),
+--     ('Red Imported Fire Ant (RIFA)'),
+--     ('Painted Apple Moth Caterpillar'),
+--     ('Pine Processionary Moth'),
+--     ('European Crane Fly'),
+--     ('Wallaby'),
+--     ('Northern Pacific Seastar'),
+--     ('Nun Moth');
